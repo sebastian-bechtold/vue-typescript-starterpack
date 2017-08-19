6c78bb32b95f5127eb1bdb9cee09d5e26cc34b0c
Sebastian Bechtold <sbecht@geomer.de>
2017-08-19 12:02:55 +0200

    Initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div class="app">
+    <p>The message: <input v-model="msg"/></p>
+    
+    <p>TypeScript and Vue say:</p>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+  props: {
+    msg: String
+  }
+})
+export default class App extends Vue {
+  msg: string;
+}
+</script>
+
+<style>
+div.app h1 {
+    font-family:sans-serif;
+}
+</style>
+
