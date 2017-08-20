dd011cd9be667933bf6c784d37f4d4e836b42dfa
Sebastian Bechtold <sbecht@geomer.de>
2017-08-20 21:46:33 +0200

    Added comments to App.vue

--- src/components/App.vue
+++ src/components/App.vue
@@ -14,14 +14,24 @@ import Component from 'vue-class-component'
 
 @Component({
   props: {
+
+    // Define 'msg' as a prop (read below for details):
     msg: String
   }
 })
 export default class App extends Vue {
 
+  // 'msg' holds the initial message that was passed as a component tag attribute, a so-called 'prop'.
+  // In the '@Component' decorator above, 'msg' is defined as a prop.
   msg : string;
+
+  // Members defined as a prop should not be modified by component code, 
+  // so we need to create another member to store the modified message:
   message: string = "";
 
+  // mounted() is automatically called when the component is added to the DOM. When this happens,
+  // we copy the initial message from the prop to a "normal" class member so that we can modify it
+  // through the input field:
   mounted() {
     this.message = this.msg;
   }
