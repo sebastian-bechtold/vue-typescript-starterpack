b9ffb2936ac8a225002d5d3c471ae74373b4d2a7
Sebastian Bechtold <sbecht@geomer.de>
2018-01-26 14:34:04 +0100

    - Changed webpack config to use "url-loader" instead of "file-loader" to load images. This solves the problem that relative URLs for e.g. CSS "background-image" are wrong in the distribution package.

--- webpack.config.js
+++ webpack.config.js
@@ -2,96 +2,87 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
 
-  /*
-      sass-loader - Transforms Sass to CSS. (Input: Sass, Output: CSS)
-      css-loader - Transforms CSS to a JavaScript module. (Input: CSS, Output: JavaScript)
-      style-loader - Injects the CSS, that is exported by the JavaScript module, into a <style> tag at runtime. (Input: JavaScript, Output: JavaScript).
-  
-      // Source: https://stackoverflow.com/questions/43913420/css-loader-vs-style-loader-vs-sass-loader
-  */
+    module: {
+        rules: [
+            {
 
-  module: {
-    rules: [
-      // TODO: Is this what we want?
-      /*
-      { 
-        test: /\.css$/, 
-        loader: "style-loader!css-loader" 
-      },
-      */
+                test: /\.(css|scss)$/,
 
-      {
-        //test: /\.scss$/,
-        test: /\.(css|scss)$/,
+                /*
+                sass-loader - Transforms Sass to CSS. (Input: Sass, Output: CSS)
+                css-loader - Transforms CSS to a JavaScript module. (Input: CSS, Output: JavaScript)
+                style-loader - Injects the CSS, that is exported by the JavaScript module, into a <style> tag at runtime. (Input: JavaScript, Output: JavaScript).
+          
+                // Source: https://stackoverflow.com/questions/43913420/css-loader-vs-style-loader-vs-sass-loader
+                */
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            },
 
-        //loaders: ["style-loader", "css-loader", "sass-loader"]
-        loaders: ["style-loader", "css-loader", "sass-loader"]
-      },
-
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          esModule: true
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    esModule: true
+                }
+            },
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                }
+            },           
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.vue'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
         }
-      },
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-        options: {
-          appendTsSuffixTo: [/\.vue$/],
-        }
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.vue'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
-  performance: {
-    hints: false
-  },
-  devtool: '#eval-source-map'
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
 }
